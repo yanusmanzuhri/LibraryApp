2c7c2e630737b22634ba1433b6e1623c4f78de64
Yayan <111275294+hypryyn@users.noreply.github.com>
2024-08-05 04:30:29 +0700

    adding styles.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 
 dependencies {
 
+    implementation 'androidx.appcompat:appcompat:1.6.1'
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
